e15f1bcafa929b0780c8bb02f00b539d7ffedc03
Nafasat Ahmed <41534389+nafasat@users.noreply.github.com>
2023-06-13 11:00:39 +0530

    add Redis connection

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 import os
+import redis
+
+try:
+    r = redis.Redis(host='34.131.18.147',port=6379, password='HdfcBank123')
+except:
+    errors.append("Unable to get URL. Please make sure it's valid and try again.")
+    return {"error": errors}
 
 app = Flask(__name__) #creating the Flask class object
 @app.route('/',methods = ['GET']) #decorator drfines the   
@@ -12,6 +19,15 @@ def port():
     PORT=os.environ['PORT']
     return PORT
 
+@app.route('/redis/set/<key>/<value>',methods = ['GET'])
+def set():
+    r.set('foo', 'bar')
+    return "Value set"
+
+@app.route('/redis/get/<key>',methods = ['GET'])
+def get():
+    value = r.get('foo')
+    return value
 
 if __name__ =='__main__':  
     app.run(host='0.0.0.0', port=5000, debug = True)
